bdd70f51a44446e9345874847b5df45611bb27a6
Sebastián Rojo <sebastianrojocollao@gmail.com>
2026-05-20 22:27:38 -0400

    Agregar archivos de configuración y estructura inicial del proyecto

---
+++ src/main/java/cl/sebastianrojo/producthub/ProducthubApiApplication.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.producthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducthubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducthubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sebastianrojo/producthub/ProducthubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.producthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducthubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
