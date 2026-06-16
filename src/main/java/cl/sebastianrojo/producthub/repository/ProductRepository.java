508b04edb3d77de68127257423279bc2eaf945be
Sebastián Rojo <sebastianrojocollao@gmail.com>
2026-06-15 20:44:55 -0400

    Agregar soporte para especificaciones en ProductRepository

--- src/main/java/cl/sebastianrojo/producthub/repository/ProductRepository.java
+++ src/main/java/cl/sebastianrojo/producthub/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package cl.sebastianrojo.producthub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import cl.sebastianrojo.producthub.entity.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository
+        extends JpaRepository<Product, Long>,
+                JpaSpecificationExecutor<Product> {
+}
