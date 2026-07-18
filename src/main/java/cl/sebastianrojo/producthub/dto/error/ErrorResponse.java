34f903c70919535367aed00205222c131b930664
Sebastián Rojo <sebastianrojocollao@gmail.com>
2026-07-18 10:08:44 -0400

    Agregar clase ErrorResponse para manejar respuestas de error en la API

---
+++ src/main/java/cl/sebastianrojo/producthub/dto/error/ErrorResponse.java
@@ -0,0 +1,15 @@
+package cl.sebastianrojo.producthub.dto.error;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+
+        LocalDateTime timestamp,
+
+        int status,
+
+        String error,
+
+        String message
+
+) {}
